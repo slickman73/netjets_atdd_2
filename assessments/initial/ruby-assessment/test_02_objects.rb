cef3a807d208bf3f10b8312d37f7dc9c3d5eeba3
slickman73 <the.tarrasque@gmail.com>
2015-05-11 12:08:16 -0400

    End Assessment One

--- assessments/initial/ruby-assessment/test_02_objects.rb
+++ assessments/initial/ruby-assessment/test_02_objects.rb
@@ -3,9 +3,8 @@ require 'helper'
 class AboutObjects < Test::Unit::TestCase
 
   def test_which_things_are_objects
-    assert_equal __, 1.is_a?(Object)
-    assert_equal __, 1.5.is_a?(Object)
-    assert_equal __, "string".is_a?(Object)
+    assert_equal true, 1.5.is_a?(Object)
+    assert_equal true, "string".is_a?(Object)
     assert_equal __, nil.is_a?(Object)
     assert_equal __, Object.is_a?(Object)
   end
--- assessments/initial/ruby-assessment/test_06_symbols.rb
+++ assessments/initial/ruby-assessment/test_06_symbols.rb
@@ -3,7 +3,7 @@ require 'helper'
 class AboutSymbols < Test::Unit::TestCase
   def test_symbol_type
     symbol = :ruby
-    assert_equal __, symbol.is_a?(Symbol)
+    assert_equal true, symbol.is_a?(Symbol)
     assert_equal __, symbol.is_a?(String)
   end
 
@@ -12,7 +12,7 @@ class AboutSymbols < Test::Unit::TestCase
     symbol2 = :a_symbol
     symbol3 = :something_else
 
-    assert_equal __, symbol1 == symbol2
+    assert_equal true, symbol1 == symbol2
     assert_equal __, symbol1 == symbol3
   end
 
