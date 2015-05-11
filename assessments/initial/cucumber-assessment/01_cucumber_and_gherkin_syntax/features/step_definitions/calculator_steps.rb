55fdb49ed0d0d92e8fa50426452e4e89566cd118
slickman73 <the.tarrasque@gmail.com>
2015-05-11 14:12:35 -0400

    Cucumber Assessment One

--- assessments/initial/cucumber-assessment/01_cucumber_and_gherkin_syntax/features/step_definitions/calculator_steps.rb
+++ assessments/initial/cucumber-assessment/01_cucumber_and_gherkin_syntax/features/step_definitions/calculator_steps.rb
@@ -1,5 +1,7 @@
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'calculator'
+require 'test/unit/assertions'
+World(Test::Unit::Assertions)
 
 Before do
   @calc = Calculator.new
@@ -24,7 +26,7 @@ Given /^I have done some arithmetic$/ do
   @calc.memadd
 end
 
-When /^I have __$/ do
+When /^I have cleared the screen$/ do
   @calc.clear
 end
 
@@ -32,8 +34,8 @@ Then /^I should see the previously stored result$/ do
   assert_equal @result, @calc.current_display
 end
 
-When /^I use the special constant __$/ do |special_constant_name|
-  @calc.push_special __
+When /^I use the special constant (.*)$/ do |constant|
+  @calc.push_special (constant)
 end
 
 Then /^the current value on the screen should be (.*)$/ do |output|
